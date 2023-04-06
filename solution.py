76f78807ff01a2c0d9e734614d31a87fbd0cd7aa
Murzik2020ai <61930598+Murzik2020ai@users.noreply.github.com>
2023-04-06 13:05:25 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,4 +8,8 @@ def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    #return x.mean() # Ваш ответ
+    df = pd.DataFrame({'value':x})
+    df['value'] = df['value'] - 779
+    df['value'] = df['value'].apply(np.log)
+    return df.value.mean()
